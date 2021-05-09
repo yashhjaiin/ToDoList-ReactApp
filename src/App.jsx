290ec24730ea8b1c840cc4444641cec4c4889ef1
Yash Jain <83462895+yashhjaiin@users.noreply.github.com>
2021-05-10 03:14:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import AddItem from './AddItems';
 import ToList from './ToList';
 
 const App = () => {
 
-    const [value, setValue] = useState("");
     const [list, setList] = useState([]);
+    const [value, setValue] = useState("");
+
+    // general technique to assign id
+    // let [count, setCount] = useState(0);
 
     const addValue = (event) => {
         setValue(event.target.value);
     }
 
     const addItem = () => {
-        setList((olist) => {
-            return [...olist, value];
-        });
-        setValue('');
+
+        // Asigning id using count by incrementing state
+        // setCount(count + 1);
+        // let tempList = list.concat({ id : count, name : value});
+
+        // Asigning id using uuid
+        if (value !== '') {
+        let tempList = list.concat({ id : uuidv4() , name : value});
+        setList(tempList);
+        setValue(''); }
+        else {
+            setList([...list]);
+        }
     }
 
     const deleteItem = (id) => {
-        console.log(id + ' deleted')
-        setList(list.filter((elem, index) => {
-            return index !== id;
+        // console.log(id + ' deleted');
+        
+        setList(list.filter((elem) => {
+            return elem.id !== id;
         }))
     }
 
@@ -29,15 +44,18 @@ const App = () => {
         <div className="upper_div">
             <div className="middle_div">                
                 <h1> To-Do List </h1>
-                <input type="text" placeholder="Add Items" onChange={addValue} value={value} required/> 
-                <button onClick={addItem}> + </button>
+                <AddItem
+                    addValue = {addValue}
+                    value = {value}
+                    addItem = {addItem}
+                 />
                 <div className='sc'>
                     <ol>
-                        {list.map((items, index) => {
+                        {list.map((items) => {
                             return <ToList
-                                    key = {index}
-                                    id = {index}
-                                    text = {items}
+                                    key = {items.id}
+                                    id = {items.id}
+                                    text = {items.name}
                                     conect = {deleteItem}
                                     />
                         })}
